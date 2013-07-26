c6c1a16224f5b940d067b28e98c661de48373471
Kasun Hewage <kasun.ch@gmail.com>
2013-07-26 13:20:15 +0200

    Initial IPI is set to 1 seconds and delay is set to 0. This causes scheduler to allocate slot immediately.

--- src/apps/lwb-mychanges/configs/default.h
+++ src/apps/lwb-mychanges/configs/default.h
@@ -17,9 +17,9 @@
 #define TX_POWER     31
 #define PRINT_PERIOD 0      // print after each round (if there's time before the next one)
 #define EXP_LENGTH   0      // the experiment has unlimited length
-#define INIT_IPI     5
+#define INIT_IPI     1
 #define INIT_PERIOD  1
-#define INIT_DELAY   300
+#define INIT_DELAY   0
 #define HOSTS        {1}
 #define SINKS        HOSTS  // the host is also the sink
 #define SOURCES      {}     // all nodes but the host are sources
