ff773f647eb766eb7a778428406b74acf4a21ac7
kasun Hewage <kasun.ch@gmail.com>
2015-03-18 12:41:08 +0100

    new stuff

--- apps/lwb-ip-test/tcp-client-server.c
+++ apps/lwb-ip-test/tcp-client-server.c
@@ -25,15 +25,16 @@
 #define UIP_IP_BUF   ((struct uip_ip_hdr *)&uip_buf[UIP_LLH_LEN])
 
 #define TCP_SERVER_NODE_ID  13
-#define TCP_CLIENT_NODE_ID  14 
-#define LWB_HOST_NODE_ID    15
+#define TCP_CLIENT_NODE_ID  2
+#define LWB_HOST_NODE_ID    1
 #define TCP_PORT            20222
 
-#define TCP_CLIENT_CONNECT_DELAY    10
+#define TCP_CLIENT_CONNECT_DELAY    90
+#define TCP_PAYLOAD_LEN             240
 
 enum {
-  ECHO_MSG_TYPE_REQ = 1,
-  ECHO_MSG_TYPE_RES
+  ECHO_MSG_TYPE_REQ = 0xBB,
+  ECHO_MSG_TYPE_RES = 0xDD
 };
 
 typedef struct __attribute__ ((__packed__)) echo_msg {
@@ -41,14 +42,12 @@ typedef struct __attribute__ ((__packed__)) echo_msg {
   uint8_t ui8_counter_l;
   uint8_t ui8_counter_h;
   uint8_t ui8_pad;
-  uint8_t dummy[26];
+  uint8_t dummy[TCP_PAYLOAD_LEN - 4];
 } echo_msg_t;
 
 typedef struct tcpstats {
   uint16_t seq;
   uint32_t lat;
-  uint32_t on;
-  uint32_t tot;  
 } tcpstats_t;
 
 #define TCPSTATS_ELEMENTS 40
@@ -60,9 +59,15 @@ static uip_ipaddr_t global_ip_addr;
 static uint16_t ui16_counter = 0;
 static echo_msg_t echo_msg_buf;
 struct psock p_socket;
-clock_time_t t_start = 0, t_end = 0; 
-uint32_t en_on_start = 0, en_on_end = 0;
-uint32_t en_tot_start = 0, en_tot_end = 0;
+uint8_t connected = 0;
+
+uint32_t en_on_start = 0;
+uint32_t en_tot_start = 0;
+uint32_t t_lat_start = 0;
+uint32_t t_lat_end = 0;
+uint32_t t_con_start = 0;
+uint32_t t_con_now = 0;
+uint32_t n_packets = 0;
 
 PROCESS(tcp_server_process, "TCP process");
 AUTOSTART_PROCESSES(&tcp_server_process);
@@ -75,13 +80,6 @@ void send_stream_req() {
     }
 }
 
-// Commented for 6LoWPAN integration
-//uint8_t lwb_tcpip_input(const uip_lladdr_t *a) {
-//    send_stream_req();
-//    lwb_queue_packet((uint8_t *)UIP_IP_BUF, uip_len, 0);
-//    return 1;
-//}
-
 uint8_t lwb_tcpip_input(uint8_t *data, uint8_t len) {
     send_stream_req();
     lwb_queue_packet(data, len, 0);
@@ -89,43 +87,41 @@ uint8_t lwb_tcpip_input(uint8_t *data, uint8_t len) {
 }
 
 void on_data(uint8_t *p_data, uint8_t ui8_len, uint16_t ui16_from_id) {
-    // Commented for 6LoWPAN integration
-    //memcpy((uint8_t *)UIP_IP_BUF, p_data, ui8_len);
-    //uip_len = ui8_len;
-    //tcpip_input();
-    sicslowpan_input(p_data, ui8_len);
+    sicslowpan_input(p_data, ui8_len, ui16_from_id);
 }
+
 
 void on_schedule_end(void) {
 
-    uint32_t en_on = 0;
-    uint32_t en_tot = 0;
-    //uint32_t dcycle = 0;
     uint8_t i = 0;
 
-    printf("P|");
-    for(; i < tcpstats_cnt; i++) {
-        en_on += tcpstats_info[i].on;
-        en_tot += tcpstats_info[i].tot;
-        printf("%u-%lu ", tcpstats_info[i].seq, (tcpstats_info[i].tot * 1000 /RTIMER_SECOND));
-    }
-    printf("\n");
+    t_con_now = clock_time();
+    n_packets += tcpstats_cnt;
 
-    //dcycle = en_on * 1e4 / en_tot;
-    //printf("%lu.%lu, ", dcycle / 100, dcycle % 100);
+    if (node_id == TCP_CLIENT_NODE_ID && connected) {
+        printf("P|");
+        for(; i < tcpstats_cnt; i++) {
+            printf("%u-%lu ", tcpstats_info[i].seq, (tcpstats_info[i].lat * 1000 / RTIMER_SECOND));
+        }
+        printf("|%lu %lu", n_packets, ((t_con_now - t_con_start) / CLOCK_SECOND)); 
+        printf("\n");
+    }
 
     tcpstats_cnt = 0;
 
-    if(node_id == TCP_SERVER_NODE_ID || node_id == TCP_CLIENT_NODE_ID) {
-        printf("T|%u %u\n", UIP_STAT(uip_stat.tcp.rexmit), UIP_STAT(uip_stat.tcp.drop));
+    if((node_id == TCP_SERVER_NODE_ID || node_id == TCP_CLIENT_NODE_ID) && connected) {
+        printf("T|%u |%lu\n", UIP_STAT(uip_stat.tcp.rexmit),
+                        ((t_con_now - t_con_start) / CLOCK_SECOND));
     }
 
-    lwb_print_stats();
+    //lwb_print_stats();
 }
 
 static PT_THREAD(server_handler())
 {
     PSOCK_BEGIN(&p_socket);
+
+    t_con_start = clock_time(); // for total time
 
     PSOCK_WAIT_UNTIL(&p_socket, PSOCK_NEWDATA(&p_socket));
     PSOCK_READBUF_LEN(&p_socket, sizeof(echo_msg_t));
@@ -153,30 +149,31 @@ static PT_THREAD(client_handler())
     PSOCK_BEGIN(&p_socket);
 
     echo_msg_buf.ui8_type = ECHO_MSG_TYPE_REQ;
+
+    t_con_start = clock_time(); // for total time
+    // for duty cycle
+    en_on_start = energest_type_time(ENERGEST_TYPE_LISTEN) + energest_type_time(ENERGEST_TYPE_TRANSMIT);
+    en_tot_start = energest_type_time(ENERGEST_TYPE_CPU) + energest_type_time(ENERGEST_TYPE_LPM);
+
     PSOCK_SEND(&p_socket, (uint8_t *) &echo_msg_buf, sizeof(echo_msg_t));
 
     while (1) {
 
-        t_start = clock_time();
-        en_on_start = energest_type_time(ENERGEST_TYPE_LISTEN) + energest_type_time(ENERGEST_TYPE_TRANSMIT);
-        en_tot_start = energest_type_time(ENERGEST_TYPE_CPU) + energest_type_time(ENERGEST_TYPE_LPM);
+        t_lat_start = energest_type_time(ENERGEST_TYPE_CPU) + energest_type_time(ENERGEST_TYPE_LPM);
 
         PSOCK_WAIT_UNTIL(&p_socket, PSOCK_NEWDATA(&p_socket));
         PSOCK_READBUF_LEN(&p_socket, sizeof(echo_msg_t));
      
-        t_end = clock_time(); 
-        en_on_end = energest_type_time(ENERGEST_TYPE_LISTEN) + energest_type_time(ENERGEST_TYPE_TRANSMIT);
-        en_tot_end = energest_type_time(ENERGEST_TYPE_CPU) + energest_type_time(ENERGEST_TYPE_LPM);
+        t_lat_end = energest_type_time(ENERGEST_TYPE_CPU) + energest_type_time(ENERGEST_TYPE_LPM);
 
-        uint16_t c = (uint16_t)echo_msg_buf.ui8_counter_l + ((uint16_t)(echo_msg_buf.ui8_counter_h) << 8);
-        uint32_t t_diff = (uint32_t)(t_end - t_start) * 1e6 / CLOCK_SECOND; // in microseconds
+        if (echo_msg_buf.ui8_type == ECHO_MSG_TYPE_RES) {
+            uint16_t c = (uint16_t)echo_msg_buf.ui8_counter_l + ((uint16_t)(echo_msg_buf.ui8_counter_h) << 8);
 
-        if (tcpstats_cnt < TCPSTATS_ELEMENTS) {
-            tcpstats_info[tcpstats_cnt].seq = c;
-            tcpstats_info[tcpstats_cnt].lat = t_diff;
-            tcpstats_info[tcpstats_cnt].on = en_on_end - en_on_start;
-            tcpstats_info[tcpstats_cnt].tot = en_tot_end - en_tot_start;
-            tcpstats_cnt++;
+            if (tcpstats_cnt < TCPSTATS_ELEMENTS) {
+                tcpstats_info[tcpstats_cnt].seq = c;
+                tcpstats_info[tcpstats_cnt].lat = t_lat_end - t_lat_start;
+                tcpstats_cnt++;
+            }
         }
     }
 
@@ -203,8 +200,6 @@ PROCESS_THREAD(tcp_server_process, ev, data)
     memcpy(&uip_lladdr.addr, ds2411_id, sizeof(uip_lladdr.addr));
 #endif // UIP_CONF_IPV6
 
-    // Commented for 6LoWPAN integration
-    //tcpip_set_outputfunc(&lwb_tcpip_input);
 
     sicslowpan_init();
     sicslowpan_set_outputfunc(lwb_tcpip_input);
@@ -247,7 +242,8 @@ PROCESS_THREAD(tcp_server_process, ev, data)
                 PROCESS_WAIT_EVENT_UNTIL(ev == tcpip_event);
     
                 if (uip_connected()) {
-                    
+
+                    connected = 1; 
                     printf("connected\n");
                     PSOCK_INIT(&p_socket, (uint8_t *) &echo_msg_buf, sizeof(echo_msg_t));
     
